0508b50b536d2235d51b44b1a1ccb427fff0f910
ksu930 <ksu930310@gmail.com>
2023-06-05 23:34:46 +0900

    Feat App.tsx 수정

--- App.tsx
+++ App.tsx
@@ -1,2 +1,19 @@
-import StorybookUIRoot from "./.ondevice/Storybook";
-export { StorybookUIRoot as default };
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={styles.header}>
+      <Text>Headedddr</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
