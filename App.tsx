c34447b372d7db99ae6da3a314c6d4cdd13d4f68
ksu930 <ksu930310@gmail.com>
2023-06-07 23:54:37 +0900

    debug #1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet, View, Text} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-function App(): JSX.Element {
+function App() {
   return (
-    <View style={styles.header}>
-      <Text className=" text-2xl text-blue-700">Hdedddr</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={styles.webview}
+        source={{uri: 'https://www.google.com/'}}
+      />
+    </SafeAreaView>
+    // <View style={styles.header}>
+    //   <Text className=" text-2xl text-blue-700">Hdedddr</Text>
+    // </View>
   );
 }
 
@@ -14,6 +22,16 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
     color: '#edc345',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  webview: {
+    flex: 1,
+    width: 300,
+    height: 300,
   },
 });
 
